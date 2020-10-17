8bfd42f19426e0065f050250091cd814167edece
ryo_kojima <kojimar24@gmail.com>
2020-10-17 17:03:41 +0900

    add project

---
+++ SwiftUI_100_knocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_100_knocks
+//
+//  Created by r-kojima on 2020/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_100_knocks/SwiftUI_100_knocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_100_knocksApp.swift
+//  SwiftUI_100_knocks
+//
+//  Created by r-kojima on 2020/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_100_knocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
