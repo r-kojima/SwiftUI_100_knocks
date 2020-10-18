9568f2b0001a1ef3b56b89792516ff0dfc0024c1
ryo_kojima <kojimar24@gmail.com>
2020-10-18 11:02:59 +0900

    Knock NavigationViews

---
+++ SwiftUI_100_knocks/NavigationViews.swift
@@ -0,0 +1,41 @@
+//
+//  NavigationViews.swift
+//  SwiftUI_100_knocks
+//
+//  Created by 小嶌諒 on 2020/10/17.
+//
+
+import SwiftUI
+
+struct HideNavigationView: View {
+    var body: some View {
+        NavigationView {
+            HStack {
+                Spacer()
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct ListAndNavigationView: View {
+    var fruits = ["Apple", "Banana", "Orange", "Grape", "Cherry", "Peach"]
+    var body: some View {
+        NavigationView {
+            List (0..<fruits.count) { index in
+                NavigationLink(destination: Text(fruits[index])) {
+                    Text(fruits[index])
+                }
+            }
+        }
+    }
+}
+
+struct NavigationViews_Previews: PreviewProvider {
+    static var previews: some View {
+        HideNavigationView()
+        ListAndNavigationView()
+    }
+}
